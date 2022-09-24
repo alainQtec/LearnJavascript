17eb3421b2c32383c7d26cab9d6d6d1de6460ebf
Alain Herve <alain.1337dev@outlook.com>
2022-09-24 17:42:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,15 +6,22 @@ function askCreds() {
     let age = prompt(`How old are you?`);
     return [name, age]
 }
-function signIn() {
-    let userName = ''
-    let userAge = '';
-    [userName, userAge] = askCreds();
-    console.log(`
-    ======================================
-    ${greet(userName)}
-    You say you are ${userAge} years old?
-    ======================================`
-    );
-}
-signIn();
+
+const user = {
+    name: 'Alain',
+    age: 22,
+    greet: function () {
+        console.log('HELLO tHERE!');
+    }
+};
+
+const listOfUsers = ['Alain', 'Herve', 'Name3', 'Name4'];
+window.console.clear();
+// window.console.error('This is an error Message!') // I hate errors.
+// console.warn('Be warned!');
+user.greet();
+console.log(listOfUsers[3]);
+listOfUsers.push('Name5', 'NAME6');
+console.log(listOfUsers);
+listOfUsers.shift();
+console.log(listOfUsers);
