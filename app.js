b7abb92744984dc72252c1ecd7734ba064ebec43
Alain Herve <alain.1337dev@outlook.com>
2022-09-24 17:54:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,20 +16,22 @@ const user = {
 };
 const funnytext = 'Bananan';
 const listOfUsers = ['Alain', 'Herve', 'Name3', 'Name4'];
+const key = true;
 window.console.clear();
-window.console.error('This is an error Message!') // I hate errors.
 // console.warn('Be warned!');
 switch (key) {
-    case value:
+    case 'bannana':
         console.log('Wow, I Love Bananas too!')
         break;
-    case value2:
+    case 'hhekek':
         console.log(listOfUsers[3]);
         listOfUsers.push('Name5', 'NAME6');
         console.log(listOfUsers);
         listOfUsers.shift();
         console.log(listOfUsers);
         console.log(listOfUsers.indexOf('Herve'));
+    case 'err':
+        window.console.error('This is an error Message!') // I hate errors.
     default:
         user.greet();
         break;
