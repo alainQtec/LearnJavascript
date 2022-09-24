5ea3b663c490bddb22b87bb72e071d4067af4fc8
Alain Herve <alain.1337dev@outlook.com>
2022-09-24 19:08:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ switch (prompt('What Kind Of food do you LIKE ?')) {
         break;
     case 'Hello':
         user.greet();
+        break;
     case 'hhekek':
         console.log(listOfUsers[3]);
         listOfUsers.push('Name5', 'NAME6');
@@ -31,10 +32,12 @@ switch (prompt('What Kind Of food do you LIKE ?')) {
         listOfUsers.shift();
         console.log(listOfUsers);
         console.log(listOfUsers.indexOf('Herve'));
+        break;
     case 'err':
         window.console.error('This is an error Message!') // I hate errors.
+        break;
     default:
-        console.log('What ?!');
+        alert('What ?!');
         break;
 }
 
