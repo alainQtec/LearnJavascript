a98e5ceaee74c59b33c190a76442949396e02316
Alain Herve <alain.1337dev@outlook.com>
2022-09-24 06:01:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 function greet(name) {
-    return `Hello ${name}`
+    return `Hello ${name}`;
+}
+function askCreds() {
+    let name = prompt(`What's your Name?`);
+    let age = prompt(`How old are you?`);
+    return [name, age]
 }
 function signIn() {
-    let userName = prompt(`What's your Name?`);
-    let userAge = prompt(`How old are you?`);
+    let userName = ''
+    let userAge = '';
+    [userName, userAge] = askCreds();
     console.log(`
-        ======================================
-        ${greet(userName)}
-        You say you are ${userAge} years old?
-        ======================================
-    `);
+    ======================================
+    ${greet(userName)}
+    You say you are ${userAge} years old?
+    ======================================`
+    );
 }
 signIn();
