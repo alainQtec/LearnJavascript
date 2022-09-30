39e23c733918e5f775ee3175ea6318c66a9544f6
Alain Herve <alain.1337dev@outlook.com>
2022-09-30 11:47:26 +0200

    Update events.js

--- events.js
+++ events.js
@@ -1,3 +1,5 @@
+const todolist = document.querySelector('#todolist');
+const items = todolist.children; //html collection
 const button = document.querySelector('.submit');
 button.addEventListener('click', function () {
     console.log('NEW ITEM ADDED.');
