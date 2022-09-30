6a7d6127e59c08ec7e96432bd01dee2978e0f3f2
Alain Herve <alain.1337dev@outlook.com>
2022-09-30 11:59:24 +0200

    Update events.js

--- events.js
+++ events.js
@@ -1,6 +1,13 @@
 const todolist = document.querySelector('#todolist');
 const items = todolist.children; //html collection
-const button = document.querySelector('.submit');
+const button = document.querySelector('.add_item');
+const counter = document.querySelector('.counter');
+counter.innerHTML = items.length
+
 button.addEventListener('click', function () {
-    console.log('NEW ITEM ADDED.');
+    let item = document.createElement('li');
+    item.classList.add('item');
+    item.innerText = 'Item 6';
+    todolist.appendChild(item);
+    counter.innerHTML = items.length
 });
