014e83b305172bcd20e8d43fe0c1e591d6f4292a
Alain Herve <alain.1337dev@outlook.com>
2022-10-01 11:52:07 +0200

    Update events.js

--- events.js
+++ events.js
@@ -1,22 +1,31 @@
-const todolist = document.querySelector('#todolist');
-const btnsBlock = document.getElementById('buttns');
-const items = todolist.children; //html collection
-const addbtn = document.querySelector('.add_item');
-const rembtn = document.querySelector('.remove_item');
-const counter = document.querySelector('.counter');
+const title = document.getElementsByClassName("title");
+const pageName = document.getElementsByClassName("todoPage");
+const todoList = document.querySelector("#todolist");
+const counter = document.querySelector(".counter");
+const addBtn = document.querySelector(".addBtn");
+const InputTxt = document.querySelector(".InputTxt");
+const items = todoList.children;
+
+function addEventListeners(item) {
+    item.addEventListener('click', function (e) {
+        e.stopPropagation(); item.remove();
+        counter.innerHTML = items.length;
+    });
+}
+
+addBtn.addEventListener('click', function (e) {
+    e.preventDefault(); // Prevent the  page from refeshing (so we wont loose our Data).
+    if (InputTxt.value != null) {
+        console.log(InputTxt.value);
+        let el = document.createElement('li'); el.className = 'item'; el.innerText = InputTxt.value;
+        todoList.appendChild(el);
+        counter.innerHTML = items.length;
+        addEventListeners(el);
+    } else {
+        console.log('Null El')
+    }
+});
+for (const item of items) {
+    addEventListeners(item);
+}
 counter.innerHTML = items.length;
-
-addbtn.addEventListener('click', function () {
-    let item = document.createElement('li');
-    item.classList.add('item');
-    item.innerText = `Item ${items.length + 1}`;
-    todolist.appendChild(item);
-    counter.innerHTML = items.length;
-    counter.classList.toggle('spectacular');
-});
-
-rembtn.addEventListener('click', function () {
-    counter.classList.toggle('spectacular');
-});
-
-//  styles
