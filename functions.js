f84403941b3dd2923804ebe95208c6692528e76c
Alain Herve <alain.1337dev@outlook.com>
2022-10-04 16:16:35 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -6,3 +6,13 @@ function askCreds() {
     let age = prompt(`How old are you?`);
     return [name, age]
 }
+let numbers = [1, 3, 5, 9, 14];
+// ASCII sorting
+numbers.sort();
+console.log(numbers);
+// numeric sorting
+numbers.sort(function (x, y) {
+    return x - y
+});
+// or
+numbers.sort((x, y) => x - y);
