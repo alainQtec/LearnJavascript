3ccd15683d1a24a3cab4b73aef133ae4c649d160
Alain Herve <alain.1337dev@outlook.com>
2022-10-09 13:18:56 +0200

    Create Resolve-EADDRINUSE.ps1

---
+++ Resolve-EADDRINUSE.ps1
@@ -0,0 +1,19 @@
+﻿function Resolve-EADDRINUSE {
+    [CmdletBinding()]
+    param (
+        # Port Number.
+        [Parameter(Mandatory = $true)][string]$Port
+    )
+
+    begin {
+        $ProcList = NETSTAT.EXE -ano | findstr :$Port
+    }
+
+    process {
+        taskkill /PID 4744 /F
+    }
+
+    end {
+
+    }
+}
