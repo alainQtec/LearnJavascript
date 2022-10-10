6cb994d34e9846cbb1723b7862dffb7ff0ac7f9b
Alain Herve <alain.1337dev@outlook.com>
2022-10-10 17:51:43 +0200

    Update Resolve-EADDRINUSE.ps1

--- public/otherScripts/Resolve-EADDRINUSE.ps1
+++ public/otherScripts/Resolve-EADDRINUSE.ps1
@@ -2,16 +2,38 @@
 # for now it's only designed  to work on windows.
 function Resolve-EADDRINUSE {
     [CmdletBinding()]
+    [Alias('FixEADR')]
     param (
         # Port Number.
         [Parameter(Mandatory = $true)][string]$Port
     )
 
     begin {
-        $ProcList = NETSTAT.EXE -ano | findstr :$Port
+        $script:getProcList = {
+            param($PortNumber)
+            return (NETSTAT.EXE -ano | findstr :$PortNumber)
+        }
     }
 
     process {
+        # $tmp = [IO.Path]::GetTempFileName(); $getProcList.Invoke($Port) | Out-File $tmp
+        $processList = $getProcList.Invoke($Port).split("\n")
+        $processList.ForEach({
+                $Array = $_.split(' ', [System.StringSplitOptions]::RemoveEmptyEntries)
+                $Array
+                # $noAct = ![string]::isnullorWhiteSpace($Array[4])
+                # $_PID = if ($noAct) { $Array[4] }else { $Array[3] }
+                # $_ACT = if ($noAct) { [string]::Empty }else { $Array[3] }
+                # [PSCustomObject]@{
+                #     IntIP = $Array[1]
+                #     ExtIP = $Array[2]
+                #     NetType = $Array[0]
+                #     Activity = $_ACT
+                #     ProcID = $_PID
+                # }
+            }
+        )
+        Write-Verbose "Found $($processList.count) running Processes that are still using the EADDR."
         taskkill /PID 4744 /F
     }
 
