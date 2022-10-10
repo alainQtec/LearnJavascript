e64700b48b121f73d69d25dcc39bc41f806b73d6
Alain Herve <alain.1337dev@outlook.com>
2022-10-10 16:13:10 +0200

    Create Resolve-EADDRINUSE.ps1

---
+++ public/otherScripts/Resolve-EADDRINUSE.ps1
@@ -0,0 +1,21 @@
+﻿# TODO: Make this script Cross-Platform
+# for now it's only designed  to work on windows.
+function Resolve-EADDRINUSE {
+    [CmdletBinding()]
+    param (
+        # Port Number.
+        [Parameter(Mandatory = $true)][string]$Port
+    )
+
+    begin {
+        $ProcList = NETSTAT.EXE -ano | findstr :$Port
+    }
+
+    process {
+        taskkill /PID 4744 /F
+    }
+
+    end {
+
+    }
+}
