a828d6fdc98d05d521295f921443e063792f8d92
Alain Herve <alain.1337dev@outlook.com>
2022-10-10 20:29:27 +0200

    Update Resolve-EADDRINUSE.ps1

--- public/otherScripts/Resolve-EADDRINUSE.ps1
+++ public/otherScripts/Resolve-EADDRINUSE.ps1
@@ -4,60 +4,61 @@
 .DESCRIPTION
     Long description
 .EXAMPLE
-    PS C:\> <example usage>
-    Explanation of what the example does
+    PS C:\> Resolve-EADDRINUSE.ps1 3000
 .INPUTS
-    Inputs (if any)
+    string[]
 .OUTPUTS
-    Output (if any)
+    none
 .NOTES
     # TODO: Make this script Cross-Platform
 #>
-    [CmdletBinding(SupportsShouldProcess)]
-    [Alias('FixEADR')]
-    param (
-        # Port Number.
-        [Parameter(Mandatory = $true)][string]$Port
-    )
+[CmdletBinding(SupportsShouldProcess = $true)]
+[Alias('FixEADR')]
+param (
+    # Port Number.
+    [Parameter(Mandatory = $true, Position = 0)]
+    [string]$Port
+)
 
-    begin {
-        $script:getProcList = {
-            param($PortNumber)
+begin {
+    $script:getProcList = {
+        param($PortNumber)
             return (NETSTAT.EXE -ano | findstr :$PortNumber)
         }
-    }
+    $tk = [IO.Path]::Combine([Environment]::GetFolderPath('System'),'taskkill.exe')
+}
 
-    process {
-        # $tmp = [IO.Path]::GetTempFileName(); $getProcList.Invoke($Port) | Out-File $tmp
-        $processList = $getProcList.Invoke($Port).split("\n")
-        $processList = $processList.ForEach({
-                $Array = $_.split(' ', [System.StringSplitOptions]::RemoveEmptyEntries)
-                $noAct = $Array[4] -eq " ";
-                $_PID = if ($noAct) { $Array[4] }else {
-                    if ($Array[3] -as 'int') {
-                        $Array[3]
-                    }else {
-                        ' '
-                    }
-                }
-                $_ACT = if ([string]::IsNullOrWhiteSpace($_PID) -or $_PID -ne $Array[3]) { [string]::Empty; $_PID = $Array[3] }else { [string]::Empty }
-                [PSCustomObject]@{
-                    # IntIP = $Array[1]
-                    # ExtIP = $Array[2]
-                    # NetType = $Array[0]
-                    Activity = $_ACT
-                    ProcID = $_PID
+process {
+    # $tmp = [IO.Path]::GetTempFileName(); $getProcList.Invoke($Port) | Out-File $tmp
+    $processList = $getProcList.Invoke($Port).split("\n")
+    $processList = $processList.ForEach({
+            $Array = $_.split(' ', [System.StringSplitOptions]::RemoveEmptyEntries)
+            $noAct = $Array[4] -eq " ";
+            $_PID = if ($noAct) { $Array[4] }else {
+                if ($Array[3] -as 'int') {
+                    $Array[3]
+                }else {
+                    ' '
                 }
             }
-        )
-        Write-Verbose "Found $($processList.count) running Processes that are still using the EADDR."
-        $Pids = $processList.ProcId | Sort-Object -Unique | Where-Object {$_ -as 'int'}
-        $Pids.ForEach({
-                if ($PSCmdlet.ShouldProcess("PID:$_", "taskkill")) {
-                    taskkill /PID $_ /F
-                }
+            $_ACT = if ([string]::IsNullOrWhiteSpace($_PID) -or $_PID -ne $Array[3]) { [string]::Empty; $_PID = $Array[3] }else { [string]::Empty }
+            [PSCustomObject]@{
+                # IntIP = $Array[1]
+                # ExtIP = $Array[2]
+                # NetType = $Array[0]
+                Activity = $_ACT
+                ProcID = $_PID
             }
-        )
-    }
+        }
+    )
+    Write-Verbose "Found $($processList.count) running Processes that are still using the EADDR."
+    $Pids = $processList.ProcId | Sort-Object -Unique | Where-Object {$_ -as 'int'}
+    $Pids.ForEach({
+            if ($PSCmdlet.ShouldProcess("PID:$_", "taskkill")) {
+                Start-Process  -FilePath $tk -ArgumentList "/PID $_ /F" -Verb runas
+            }
+        }
+    )
+}
 
-    end {}
+end {}
