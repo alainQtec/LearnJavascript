98ff35608ab9a7459b6dc1d06902c381b3a79986
Alain Herve <alain.1337dev@outlook.com>
2022-10-10 18:03:36 +0200

    Update Resolve-EADDRINUSE.ps1

--- public/otherScripts/Resolve-EADDRINUSE.ps1
+++ public/otherScripts/Resolve-EADDRINUSE.ps1
@@ -18,19 +18,24 @@ function Resolve-EADDRINUSE {
     process {
         # $tmp = [IO.Path]::GetTempFileName(); $getProcList.Invoke($Port) | Out-File $tmp
         $processList = $getProcList.Invoke($Port).split("\n")
-        $processList.ForEach({
+        $processList = $processList.ForEach({
                 $Array = $_.split(' ', [System.StringSplitOptions]::RemoveEmptyEntries)
-                $Array
-                # $noAct = ![string]::isnullorWhiteSpace($Array[4])
-                # $_PID = if ($noAct) { $Array[4] }else { $Array[3] }
-                # $_ACT = if ($noAct) { [string]::Empty }else { $Array[3] }
-                # [PSCustomObject]@{
-                #     IntIP = $Array[1]
-                #     ExtIP = $Array[2]
-                #     NetType = $Array[0]
-                #     Activity = $_ACT
-                #     ProcID = $_PID
-                # }
+                $noAct = $Array[4] -eq " ";
+                $_PID = if ($noAct) { $Array[4] }else {
+                    if ($Array[3] -is 'int') {
+                        $Array[3]
+                    }else {
+                        [string]::Empty
+                    }
+                }
+                $_ACT = if ($_PID -eq $Array[3]) { [string]::Empty }else { $Array[3] }
+                [PSCustomObject]@{
+                    IntIP = $Array[1]
+                    ExtIP = $Array[2]
+                    NetType = $Array[0]
+                    Activity = $_ACT
+                    ProcID = $_PID
+                }
             }
         )
         Write-Verbose "Found $($processList.count) running Processes that are still using the EADDR."
