0bd9b23c0a385b0bfdfecc0795aeac03384eef99
Alain Herve <alain.1337dev@outlook.com>
2022-10-24 18:04:33 +0200

    Create index.js

---
+++ public/javascripts/Basics/arrays/index.js
@@ -0,0 +1,36 @@
+// sample array of students
+const students = [
+  {
+    name: "Jill",
+    lastname: "Doe",
+    age: 24,
+    course: "Marketing",
+  },
+  {
+    name: "John",
+    lastname: "Doe",
+    age: 22,
+    course: "Web Development",
+  },
+  {
+    name: "Jack",
+    lastname: "Doe",
+    age: 20,
+    course: "Accounting",
+  },
+  {
+    name: "Ryan Jhon",
+    lastname: "Ray",
+    age: 23,
+    course: "Web Development",
+  },
+  {
+    name: "Jane",
+    lastname: "Doe",
+    age: 20,
+    course: "Financial Management",
+  },
+];
+
+// Write your code here!
+console.log(students);
